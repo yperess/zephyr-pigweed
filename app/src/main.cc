ee6e94af150faba0c59f06f051c83d1d367547fb
Yuval Peress <peress@google.com>
2024-01-04 10:08:13 -0700

    Enable tokenized logging

--- app/src/main.cc
+++ app/src/main.cc
@@ -3,9 +3,16 @@
 
 #define PW_LOG_MODULE_NAME "codelab"
 
+#include "pw_chrono/system_clock.h"
 #include "pw_log/log.h"
+#include "pw_thread/sleep.h"
+
+using namespace std::chrono_literals;
 
 int main() {
-  PW_LOG_INFO("Hello %s", CONFIG_BOARD);
-  return 0;
+  while (true) {
+    PW_LOG_INFO("Hello %s", CONFIG_BOARD);
+    pw::this_thread::sleep_for(1s);
+  }
+  PW_UNREACHABLE;
 }
