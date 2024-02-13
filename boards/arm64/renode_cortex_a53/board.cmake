ba8c3896f6dc55563347d25ad790c2260a3d826e
Yuval Peress <peress@google.com>
2024-02-12 23:05:05 -0700

    Renode: add renode_cortex_a53 board to tests

---
+++ boards/arm64/renode_cortex_a53/board.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2024 Google LLC, Antmicro
+# SPDX-License-Identifier: Apache-2.0
+
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/cortex_a53.resc)
+set(RENODE_UART sysbus.uart0)
